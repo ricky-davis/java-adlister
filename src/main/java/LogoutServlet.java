2965c9e9f223c2ce1f8da7eda51bc7ff73e97376
Ricky Davis <ricky.c.davis.9@gmail.com>
2019-06-13 11:54:25 -0500

    Properly invalidating session on logout

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getSession().removeAttribute("user");
+        req.getSession().invalidate();
         resp.sendRedirect("/login");
     }
 }
